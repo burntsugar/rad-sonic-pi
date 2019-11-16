c168b1076848d972e6454dd7bd16e99756645f1c
burntsugar <rrakky89@gmail.com>
2019-11-17 10:49:48 +1100

    improved teh beats

--- flume-1.rb
+++ flume-1.rb
@@ -4,18 +4,47 @@ flume = "~/my-sonic-pi/sonic-samples/Flume Sounds 1/"
 
 use_bpm 60
 
-rel = 1
 fin = 0.675
 
 print sample_duration flume, 2, start: 0.640, finish: fin, rate: 0.5
-print sample_duration flume, 2, start: 0.625, finish: fin, rate: 0.5, amp: 2, release: 1
-boomp_duration = 1.0 + 2.46 + 2.52 + 1
-puts boomp_duration
+print sample_duration flume, 2, start: 0.625, finish: 0.6562, rate: 0.5
+print sample_duration flume, 2, slice: 30, rate: 5, attack: 2
+print sample_duration flume, 2, slice: 15
+print sample_duration flume, 2, slice: 55, rate: 5
+print sample_duration flume, 6, amp: 2, start: 0.599, finish: 0.79, rate: 0.90, release: 2
 
 live_loop :boomp do
-  sleep 1
-  sample flume, 2, start: 0.640, finish: fin, rate: 0.5
-  sleep 2.46
-  sample flume, 2, start: 0.625, finish: fin, rate: 0.5, amp: 2, release: 1
-  sleep 2.52
+  puts "terp"
+  1.times do
+    sleep 1
+    boomp_dur += 1
+    
+    sample flume, 2, start: 0.640, finish: fin, rate: 0.5
+    sleep sample_duration flume, 2, start: 0.640, finish: fin, rate: 0.5
+    
+    sample flume, 2, start: 0.625, finish: 0.6562, rate: 0.5
+    sleep sample_duration flume, 2, start: 0.625, finish: 0.6562, rate: 0.5
+    
+    sample flume, 2, start: 0.640, finish: fin, rate: 0.5
+    sleep sample_duration flume, 2, start: 0.640, finish: fin, rate: 0.5
+    
+    sleep 0.2
+    sample flume, 2, slice: 30, rate: 5, attack: 2
+    
+    sample flume, 2, start: 0.625, finish: 0.6562, rate: 0.5, amp: 2
+    sleep sample_duration flume, 2, start: 0.625, finish: 0.6562, rate: 0.5, amp: 2
+    
+    sample flume, 2, slice: 15
+    sample flume, 2, slice: 55, rate: 5
+    sleep (sample_duration flume, 2, slice: 15) -1
+  end
+  with_fx :reverb, room: 1 do
+    sleep 1
+    sample flume, 2, slice: 30, rate: 5, attack: 2
+    sleep 1
+    sample flume, 6, amp: 2, start: 0.599, finish: 0.79, rate: 0.90, release: 2
+    sleep 0.10
+    sample flume, 6, amp: 2, start: 0.599, finish: 0.79, rate: 0.90, release: 2
+    sleep (sample_duration flume, 6, amp: 2, start: 0.599, finish: 0.79, rate: 0.90) - 1.5
+  end
 end
