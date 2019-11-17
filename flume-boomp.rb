04b5af90ba49976c531516ce4eb7a6f225b05b80
burntsugar <rrakky89@gmail.com>
2019-11-17 11:50:59 +1100

    clean teh codez

--- flume-boomp.rb
+++ flume-boomp.rb
@@ -3,21 +3,20 @@
 flume = "~/my-sonic-pi/sonic-samples/Flume Sounds 1/"
 
 use_bpm 60
-
 fin = 0.675
-
 print sample_duration flume, 2, start: 0.640, finish: fin, rate: 0.5
 print sample_duration flume, 2, start: 0.625, finish: 0.6562, rate: 0.5
 print sample_duration flume, 2, slice: 30, rate: 5, attack: 2
 print sample_duration flume, 2, slice: 15
 print sample_duration flume, 2, slice: 55, rate: 5
 print sample_duration flume, 6, amp: 2, start: 0.599, finish: 0.79, rate: 0.90, release: 2
+timez = 1
 
 live_loop :boomp do
   puts "terp"
-  1.times do
+  
+  timez.times do
     sleep 1
-    boomp_dur += 1
     
     sample flume, 2, start: 0.640, finish: fin, rate: 0.5
     sleep sample_duration flume, 2, start: 0.640, finish: fin, rate: 0.5
@@ -38,13 +37,18 @@ live_loop :boomp do
     sample flume, 2, slice: 55, rate: 5
     sleep (sample_duration flume, 2, slice: 15) -1
   end
+  
   with_fx :reverb, room: 1 do
     sleep 2
+    
     sample flume, 2, slice: 30, rate: 4, pre_amp: 1, amp: 2, attack: 2
     sleep 2
+    
     sample flume, 6, amp: 2, start: 0.599, finish: 0.79, rate: 0.90, release: 2
     sleep 0.10
+    
     sample flume, 6, amp: 2, start: 0.599, finish: 0.79, rate: 0.90, release: 2
     sleep (sample_duration flume, 6, amp: 2, start: 0.599, finish: 0.79, rate: 0.90) - 1.5
   end
+  
 end
